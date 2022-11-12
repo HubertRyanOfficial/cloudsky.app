f6e05216c8b71ed2aaa6b28eaf8f3740279b7c58
Hubert Ryan <hubertryanofficial@gmail.com>
2022-11-12 04:34:18 -0300

    loading component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { Player } from "@lottiefiles/react-lottie-player";
+
 import App from "../components/App";
 
 import Logo from "../public/logo.png";
+import LoadingAnimation from "../public/animations/98092-loading.json";
 
 export default function Home() {
   return (
@@ -23,6 +26,7 @@ function Loading() {
   return (
     <div className={styles.loadingContainer}>
       <Image width={200} src={Logo} alt="YouNoty" />
+      <Player src={LoadingAnimation} autoplay loop style={{ width: 300 }} />
     </div>
   );
 }
