11968574390b0d60b5b15c4d2ed0b4f6ca613002
Hubert Ryan <hubertryanofficial@gmail.com>
2022-11-12 05:33:53 -0300

    input style added

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import styles from "./Board.module.css";
 
 interface Props {}
 
 const Board = ({}: Props) => {
-  return <div className={styles.container}></div>;
+  const [title, setTitle] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputContainer}>
+        <div className={styles.inputContent}>
+          <input
+            value={title}
+            className={styles.inputItem}
+            maxLength={50}
+            placeholder="Nova nota"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <span>EXCLUIR</span>
+        </div>
+        <div className={styles.separator} />
+      </div>
+    </div>
+  );
 };
 
 export default Board;
