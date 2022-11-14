afe83ab69380b1e8458b67a3b92eb20a6812b8fd
Hubert Ryan <hubertryanofficial@gmail.com>
2022-11-14 13:48:26 -0300

    animations improvements to choose tag

--- components/SelectTag/index.tsx
+++ components/SelectTag/index.tsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
+import { BiX } from "react-icons/bi";
 import styles from "./SelectTag.module.css";
 
 export default function SelectTag() {
-  const [showTags, setShowTags] = useState(true);
+  const [showTags, setShowTags] = useState(false);
 
   return (
-    <div>
+    <div className={styles.content}>
       <motion.div
-        initial={{ opacity: 0, width: 180 }}
-        animate={{ width: [180, 200], opacity: 1 }}
+        initial={{ opacity: 0, width: 180, height: 40 }}
+        animate={{
+          width: [!showTags ? 180 : 200, !showTags ? 200 : 40],
+          opacity: 1,
+        }}
         whileTap={{ scale: 0.9 }}
         className={styles.container}
+        onClick={() => setShowTags(!showTags)}
       >
-        <span>Selecionar projeto</span>
+        {!showTags ? (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: !showTags ? 1 : 0 }}
+          >
+            Selecionar projeto
+          </motion.span>
+        ) : (
+          <BiX size={22} color="#ff4f4b" />
+        )}
       </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: showTags ? 1 : 0, x: showTags ? 0 : -20 }}
+        className={styles.tagsContainer}
+      >
+        <TagItem />
+        <TagItem />
+        <TagItem />
+        <TagItem />
+      </motion.div>
+    </div>
+  );
+}
+
+function TagItem() {
+  return (
+    <div className={styles.tagItem}>
+      <strong>Tapedin</strong>
     </div>
   );
 }
