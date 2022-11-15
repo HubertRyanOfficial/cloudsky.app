1d9843728068055771f7ade99a4047ae4b800547
Hubert Ryan <hubertryanofficial@gmail.com>
2022-11-15 11:16:55 -0300

    improvements in cloudsky remove tag

--- components/SelectTag/index.tsx
+++ components/SelectTag/index.tsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import { useApp } from "../../context/AppContext";
 
-import { BiX, BiCheck } from "react-icons/bi";
+import { BiX, BiCheck, BiUnlink } from "react-icons/bi";
 import styles from "./SelectTag.module.css";
 
 export default function SelectTag() {
@@ -70,6 +70,29 @@ export default function SelectTag() {
           <BiX size={22} color="#ff4f4b" />
         )}
       </motion.div>
+
+      <AnimatePresence>
+        {showTags && !newProject && tagSelected && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            whileHover={{ opacity: 0.5 }}
+            whileTap={{ scale: 0.9 }}
+            className={styles.clearContent}
+            onClick={() => handleSelectTag(null)}
+          >
+            <BiUnlink size={22} color="#ff4f4b" />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <AnimatePresence>
         {!newProject && showTags && (
@@ -146,29 +169,6 @@ export default function SelectTag() {
           >
             <BiCheck size={22} color="#ff4f4b" />
           </motion.div>
-        )}
-      </AnimatePresence>
-
-      <AnimatePresence>
-        {showTags && !newProject && tagSelected && (
-          <motion.span
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-            }}
-            exit={{
-              opacity: 0,
-            }}
-            transition={{
-              duration: 0.5,
-            }}
-            whileHover={{ opacity: 0.5 }}
-            whileTap={{ scale: 0.9 }}
-            className={styles.clearContent}
-            onClick={() => handleSelectTag(null)}
-          >
-            Limpar
-          </motion.span>
         )}
       </AnimatePresence>
     </div>
